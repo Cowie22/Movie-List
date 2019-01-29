4a956ad6a7604c168383a80651c67c7997814d70
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-28 20:57:24 -0800

    Added watched toggle btn

--- src/Components/Movie-List-Entry.jsx
+++ src/Components/Movie-List-Entry.jsx
@@ -1,9 +1,35 @@
-var MovieListEntry = function(props) {
-  return (
-    <div>
-      <span className='movieListEntry'>{props.movie.title}</span>
-    </div>
-  )
+// var MovieListEntry = function(props) {
+//   return (
+//     <div>
+//       <span className='movieListEntry'>{props.movie.title}</span>
+//       <button>Watched</button>
+//     </div>
+//   )
+// }
+
+class MovieListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btnToggled: true
+    }
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+  }
+  handleBtnClick() {
+    this.setState(state => ({
+      btnToggled: !state.btnToggled
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <span className='movieListEntry'>{this.props.movie.title} 
+        <button className="watchBTN" onClick={this.handleBtnClick}>
+        {this.state.btnToggled ? 'Not Watched' : 'Watched'}</button>
+        </span>  
+      </div>
+    )
+  }
 }
 
 export default MovieListEntry
